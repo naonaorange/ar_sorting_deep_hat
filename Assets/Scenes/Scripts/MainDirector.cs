8a50e394b600f7ebb7a0329d53d55d166f7c4534
naonaorange <nao.lunalovegood@gmail.com>
2020-04-29 00:09:45 +0900

    show face rect image

--- Assets/Scenes/Scripts/MainDirector.cs
+++ Assets/Scenes/Scripts/MainDirector.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using GoogleARCore;
 using GoogleARCore.Examples.AugmentedFaces;
 using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.ImgprocModule;
+using OpenCVForUnity.ImgcodecsModule;
 using OpenCVForUnity.UnityUtils;
 
 public class MainDirector : MonoBehaviour
@@ -15,9 +17,10 @@ public class MainDirector : MonoBehaviour
 
     private Text logText1;
     private Text logText2;
+    private Text logText3;
     private RawImage camImg;
 
-    private float timeSpan = 1.0f;
+    private float timeSpan = 3.0f;
     private float timeDelta = 0;
 
     // Start is called before the first frame update
@@ -29,6 +32,7 @@ public class MainDirector : MonoBehaviour
 
         this.logText1 = GameObject.Find("LogText1").GetComponent<Text>();
         this.logText2 = GameObject.Find("LogText2").GetComponent<Text>();
+        this.logText3 = GameObject.Find("LogText3").GetComponent<Text>();
         this.camImg = GameObject.Find("CameraImage").GetComponent<RawImage>();
     }
 
@@ -52,38 +56,67 @@ public class MainDirector : MonoBehaviour
 
             if (isProcessOk)
             {
-                UnityEngine.Rect faceRect = this.DetectFaceRectInScreen();
 
                 Texture2D texture = this.textureRender.FrameTexture;
                 if (texture == null) return;
 
-                this.camImg.texture = texture;
-
                 Mat srcImg = new Mat(texture.height, texture.width, CvType.CV_8UC3);
                 Utils.texture2DToMat(texture, srcImg);
 
-                this.logText1.GetComponent<Text>().text = srcImg.width() + "  " + srcImg.height() + "  " + srcImg.depth();
-                double[] p = srcImg.get(0, 0);
-                this.logText2.GetComponent<Text>().text = p[0].ToString("F2") + "  " + p[1].ToString("F2") + "  " + p[2].ToString("F2");
-
                 /*
-                Texture2D dispTexture = new Texture2D(srcImg.width(), srcImg.height());
-                Utils.matToTexture2D(srcImg, dispTexture);
+                Mat dispImg = new Mat(texture.height, texture.width, CvType.CV_8UC3);
+                Core.rotate(srcImg, dispImg, Core.ROTATE_90_COUNTERCLOCKWISE);
+                */
+                Core.rotate(srcImg, srcImg, Core.ROTATE_90_COUNTERCLOCKWISE);
 
-                //this.logText1.GetComponent<Text>().text = texture.width + "  " + texture.height;
+                OpenCVForUnity.CoreModule.Rect faceRect = this.DetectFaceRectInImage(srcImg.height(), srcImg.width());
+                /*
+                this.logText1.GetComponent<Text>().text = faceRect.ToString();
+                this.logText2.GetComponent<Text>().text = Screen.width + "  " + Screen.height;
+                this.logText3.GetComponent<Text>().text = arCamera.pixelWidth + "  " + arCamera.pixelHeight;
+                Imgproc.rectangle(dispImg, faceRect, new Scalar(0, 0, 200), 3, 4);
+                Texture2D dispTexture = new Texture2D(dispImg.width(), dispImg.height(), TextureFormat.RGBA32, false);
+                Utils.matToTexture2D(dispImg, dispTexture);
                 */
 
+                Mat faceImg = new Mat(srcImg, faceRect);
+                
+                Texture2D dispTexture = new Texture2D(faceImg.width(), faceImg.height(), TextureFormat.RGBA32, false);
+                Utils.matToTexture2D(faceImg, dispTexture);
+                
+                this.camImg.texture = dispTexture;
+
                 srcImg.Dispose();
+                //dispImg.Dispose();
+                faceImg.Dispose();
             }
         else
         {
             this.logText1.text = "";
+            this.logText2.text = "";
+            this.logText3.text = "";
         }
 
         }
     }
 
-    private UnityEngine.Rect DetectFaceRectInScreen()
+    private OpenCVForUnity.CoreModule.Rect DetectFaceRectInImage(int height, int width)
+    {
+        var rect = DetectFaceRect();
+
+        float wScale = (float)width / Screen.width;
+        float hScale = (float)height / Screen.height;
+
+        float x = rect.x * wScale;
+        float y = (Screen.height - rect.y) * hScale;
+        float w = rect.width * wScale;
+        float h = rect.height * hScale;
+
+        return new OpenCVForUnity.CoreModule.Rect((int)x, (int)y, (int)w, (int)h);
+
+    }
+
+    private OpenCVForUnity.CoreModule.Rect DetectFaceRect()
     {
         var faceTrackComp = this.faceTrack.GetComponent<ARCoreAugmentedFaceRig>();
         var arCameraComp = this.arCamera.GetComponent<Camera>();
@@ -98,13 +131,14 @@ public class MainDirector : MonoBehaviour
         headLeftPos = arCameraComp.WorldToScreenPoint(headLeftPos);
         headRightPos = arCameraComp.WorldToScreenPoint(headRightPos);
 
-        float width = headRightPos.x - headLeftPos.x;
-        float height = headLeftPos.y - nosePos.y;
+        float scale = 1.0f;
+        float width = (headRightPos.x - headLeftPos.x) * scale;
+        float height = ((headLeftPos.y - nosePos.y) * 2) * scale;
 
-        UnityEngine.Rect rect = new UnityEngine.Rect(   centerPos.x - width,
-                                centerPos.y - height,
-                                width,
-                                height);
-        return rect;
+        return new OpenCVForUnity.CoreModule.Rect(  (int)(centerPos.x - height / 2),
+                                                    (int)(centerPos.y + height / 2),
+                                                    (int)height,
+                                                    (int)height);
     }
+
 }
